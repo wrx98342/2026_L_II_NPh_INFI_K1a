80556c532425d8551a59f3fdd7344b22082c6bcd
pb <pbalzer1997@wp.pl>
2026-03-17 18:55:55 +0100

    Poprawa JSON

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -21,8 +21,7 @@ def get_formatted(msg, imie, format):
 
 
 def format_to_json(msg, imie):
-    return ('{ "imie":"' + imie + '", "mgs":' +
-            msg + '"}')
+    return '{"imie": "' + imie + '", "msg": "' + msg + '"}'
 
 
 def plain_text(msg, imie):
