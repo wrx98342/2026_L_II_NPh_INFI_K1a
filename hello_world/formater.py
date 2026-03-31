3b73f421e23363b56b694fb8a4c52be9675663d0
wrx98342 <wrx98342@student.merito.wroclaw.pl>
2026-03-31 18:18:45 +0200

    Bug Formater

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -28,9 +28,9 @@ def plain_text(msg, imie):
     return imie + ' ' + msg
 
 
-def plain_text_upper_case(msg, imie):
+def plain_text_lower_case(msg, imie):
     return plain_text(msg.upper(), imie.upper())
 
 
-def plain_text_lower_case(msg, imie):
+def plain_text_upper_case(msg, imie):
     return plain_text(msg.lower(), imie.lower())
